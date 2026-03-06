ec7a789ca2ffa5e10d24b0d86007acfef05edfb0
alejandrosoto <soto.its@gmail.com>
2026-03-05 18:57:35 -0600

    5-mar

--- lib/image_widget.dart
+++ lib/image_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({super.key});
@@ -7,12 +8,36 @@ class ImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ejemplo Imagenes"),
+        title: Text("Motocross App",style: GoogleFonts.blackOpsOne(color: Colors.white, fontSize: 30),),
+        backgroundColor: Colors.redAccent,
+        foregroundColor: Colors.white,
       ),
       body: Container(
-        height: 500,
-        width: 300,
-        child: Image.asset('assets/img/fondo.jpg'),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/fondo.jpg'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Center(
+          child: Container(
+            height: 150,
+            width: double.infinity,
+              color: Colors.black54,
+              child: Center(
+              child: Text(
+                "Bienvenido a Motocross App",
+                style: GoogleFonts.blackOpsOne(
+                    color: Colors.white,
+                    fontSize: 36
+                ),
+                textAlign: TextAlign.center,
+              ),
+          ),
+          ),
+        )
       ),
     );
   }
