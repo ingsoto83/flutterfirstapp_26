c9f5875df24feb9c681ba58cedde9e256cbfbb42
alejandrosoto <soto.its@gmail.com>
2026-03-19 18:52:58 -0600

    19-mar

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -7,6 +7,7 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     Widget imageSection = Image.asset('assets/img/event.jpg',width: size.width, height: size.height * 0.30, fit: BoxFit.cover,);
+    Widget imageSectionL = Image.asset('assets/img/event.jpg',width: size.width * 0.40, height: size.height, fit: BoxFit.cover,);
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -36,14 +37,39 @@ class DetailPage extends StatelessWidget {
         _buildButtonColumn(Colors.blue, Icons.share, 'SHARE'),
       ],
     );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child:Text('In volutpat leo a odio molestie, vel posuere leo blandit. Etiam dictum sed dui eget pellentesque. Vivamus vestibulum imperdiet cursus. Aliquam erat volutpat. Maecenas tincidunt neque odio, vel pharetra justo dignissim consectetur. Mauris aliquam, mi a pellentesque cursus, ante tellus dictum justo, nec malesuada magna orci ut odio. Maecenas et interdum tortor. Ut commodo tortor a molestie suscipit. Mauris facilisis, tortor sed lobortis mollis, dui elit iaculis metus, sed fringilla velit justo vitae augue. Cras in feugiat mauris. Fusce iaculis nulla tincidunt, suscipit dui vitae, suscipit metus. Aliquam sit amet tortor porta, convallis sem vel, vestibulum lectus. In viverra luctus nibh vitae efficitur. Proin vulputate tortor erat, a accumsan ipsum scelerisque a. Duis molestie nisl at ipsum gravida hendrerit.',
+        softWrap: true,textAlign: TextAlign.justify,)
+    );
+
     return Scaffold(
-     body: Column(
-       children: [
-         imageSection,
-         titleSection,
-         buttonSection
-       ],
-     )
+     body: OrientationBuilder(builder: (context, orientation){
+       return orientation == Orientation.portrait ?
+       SingleChildScrollView(
+         child: Column(
+           children: [
+             imageSection,
+             titleSection,
+             buttonSection,
+             textSection
+           ],
+         ),
+       ) : Row(
+         children: [
+           imageSectionL,
+           Expanded(
+             child: ListView(
+               children: [
+                 titleSection,
+                 buttonSection,
+                 textSection
+               ],
+             ),
+           )
+         ]
+       );
+     })
     );
   }
 
